6775c1248108fd2d58fd76982ade7c9522d09680
Golovko <golovcozlata@gmail.com>
2017-07-16 14:13:24 +0300

    parserC

--- src/main/java/home/parser/golovko/HtmlParser.java
+++ src/main/java/home/parser/golovko/HtmlParser.java
@@ -11,6 +11,23 @@ public class HtmlParser implements ParserAction {
     private String source;
 
     public List<String> parseCssQuery(String query) {
+
+        int i = source.indexOf("</button>");
+        String s = source.substring(0,i);
+
+        int ii = s.indexOf("<button");
+        String ss = s.substring(ii +8, i);
+
+
+        int dlinaSource = source.length();
+        int dlina1 = source.indexOf("</button>")+9;
+        String rez = source.substring(dlina1,dlinaSource);
+
+        int kon = rez.indexOf("</button>");
+        //String s2 = rez.substring(dlina1, dlinaSource);
+        int ii2 = rez.indexOf("<button");
+        String ss2 = rez.substring(ii2 +8, kon);
+
         return new ArrayList<String>();
     }
 
